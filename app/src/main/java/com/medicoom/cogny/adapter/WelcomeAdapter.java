95a507209771fd70bcc42299022806046c946dc1
pabitboul <pablo.abitboul@gmail.com>
2017-02-21 18:12:34 -0300

    -WelcomeFragment recycler view with animation

--- app/src/main/java/com/medicoom/cogny/adapter/WelcomeAdapter.java
+++ app/src/main/java/com/medicoom/cogny/adapter/WelcomeAdapter.java
@@ -1,21 +1,32 @@
 package com.medicoom.cogny.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.medicoom.cogny.R;
 import com.medicoom.cogny.model.WelcomeItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WelcomeAdapter extends RecyclerView.Adapter<WelcomeAdapter.WelcomeViewHolder> {
 
     private List<WelcomeItem> mWelcomeItemList;
+    private Context mContext;
+    private int mLastPosition;
+    private WelcomeAdapterListener mListener;
+
+    public interface WelcomeAdapterListener{
+        void onItemAdded();
+    }
 
     @Override
     public WelcomeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -24,8 +35,24 @@ public class WelcomeAdapter extends RecyclerView.Adapter<WelcomeAdapter.WelcomeV
         return viewHolder;
     }
 
-    public WelcomeAdapter(List<WelcomeItem> list) {
+    public WelcomeAdapter(List<WelcomeItem> list, Context context, WelcomeAdapterListener listener) {
         mWelcomeItemList = list;
+        mContext = context;
+        mListener = listener;
+        mLastPosition = -1;
+    }
+
+    public WelcomeAdapter(WelcomeAdapterListener listener, Context context) {
+        mWelcomeItemList = new ArrayList<>();
+        mContext = context;
+        mListener = listener;
+        mLastPosition = -1;
+    }
+
+    public void addItem(WelcomeItem welcomeItem) {
+        mWelcomeItemList.add(welcomeItem);
+        notifyDataSetChanged();
+        mListener.onItemAdded();
     }
 
     @Override
@@ -33,6 +60,8 @@ public class WelcomeAdapter extends RecyclerView.Adapter<WelcomeAdapter.WelcomeV
         viewHolder.title.setText(mWelcomeItemList.get(position).getTitle());
         viewHolder.body.setText(mWelcomeItemList.get(position).getBody());
         viewHolder.image.setImageResource(mWelcomeItemList.get(position).getIcon());
+
+        setAnimation(viewHolder.cv, position);
     }
 
     @Override
@@ -48,10 +77,19 @@ public class WelcomeAdapter extends RecyclerView.Adapter<WelcomeAdapter.WelcomeV
 
         WelcomeViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.card_view);
-            title = (TextView)itemView.findViewById(R.id.card_title);
-            body = (TextView)itemView.findViewById(R.id.card_body);
-            image = (ImageView)itemView.findViewById(R.id.card_image);
+            cv = (CardView) itemView.findViewById(R.id.card_view);
+            title = (TextView) itemView.findViewById(R.id.card_title);
+            body = (TextView) itemView.findViewById(R.id.card_body);
+            image = (ImageView) itemView.findViewById(R.id.card_image);
+        }
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > mLastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(mContext, android.R.anim.slide_in_left);
+            animation.setDuration(1000);
+            viewToAnimate.startAnimation(animation);
+            mLastPosition = position;
         }
     }
 }
--- app/src/main/java/com/medicoom/cogny/fragment/WelcomeFragment.java
+++ app/src/main/java/com/medicoom/cogny/fragment/WelcomeFragment.java
@@ -1,5 +1,6 @@
 package com.medicoom.cogny.fragment;
 
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,22 +8,39 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.medicoom.cogny.R;
 import com.medicoom.cogny.adapter.WelcomeAdapter;
 import com.medicoom.cogny.model.WelcomeItem;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.CARBOHYDRATES;
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.DEFAULT;
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.DIET;
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.EXERCISE;
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.MEDICINE;
+import static com.medicoom.cogny.fragment.WelcomeFragment.WelcomeItemType.MOOD;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class WelcomeFragment extends Fragment {
+public class WelcomeFragment extends Fragment implements WelcomeAdapter.WelcomeAdapterListener {
     private RecyclerView rvItems;
     private LinearLayoutManager rvItemsLayoutManager;
+    private WelcomeAdapter mWelcomeAdapter;
+    private Runnable mRunnable;
+    private Handler mHandler;
+
+    interface WelcomeItemType {
+        int DEFAULT = 0;
+        int MEDICINE = 1;
+        int DIET = 2;
+        int CARBOHYDRATES = 3;
+        int EXERCISE = 4;
+        int MOOD  = 5;
+    }
 
     public WelcomeFragment() {
     }
@@ -40,53 +58,83 @@ public class WelcomeFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        WelcomeAdapter welcomeAdapter = new WelcomeAdapter(createItems());
+        /*WelcomeAdapter welcomeAdapter = new WelcomeAdapter(createItems(), getActivity());
         rvItems.setHasFixedSize(true);
         rvItems.setLayoutManager(rvItemsLayoutManager);
         rvItems.setAdapter(welcomeAdapter);
-        welcomeAdapter.notifyDataSetChanged();
+        welcomeAdapter.notifyDataSetChanged();*/
+
+        mWelcomeAdapter = new WelcomeAdapter(this, getActivity());
+        rvItems.setHasFixedSize(true);
+        rvItems.setLayoutManager(rvItemsLayoutManager);
+        rvItems.setAdapter(mWelcomeAdapter);
+        mWelcomeAdapter.addItem(createWelcomeItem(DEFAULT));
+
     }
 
-    private ArrayList<WelcomeItem> createItems(){
-        ArrayList<WelcomeItem> welcomeItemArrayList = new ArrayList<>();
+    private WelcomeItem createWelcomeItem(int type) {
         WelcomeItem itemWelcome = new WelcomeItem();
-        WelcomeItem itemMedicine = new WelcomeItem();
-        WelcomeItem itemDiet = new WelcomeItem();
-        WelcomeItem itemCarbohydrates = new WelcomeItem();
-        WelcomeItem itemExercise = new WelcomeItem();
-        WelcomeItem itemMood = new WelcomeItem();
+        switch (type){
+            case MEDICINE:
+                itemWelcome.setTitle(getString(R.string.medication_title));
+                itemWelcome.setBody(getString(R.string.medication_body));
+                itemWelcome.setIcon(R.drawable.ic_medicine);
+                break;
+            case DIET:
+                itemWelcome.setTitle(getString(R.string.diet_title));
+                itemWelcome.setBody(getString(R.string.diet_body));
+                itemWelcome.setIcon(R.drawable.ic_food);
+                break;
+            case CARBOHYDRATES:
+                itemWelcome.setTitle(getString(R.string.carbohydrates_title));
+                itemWelcome.setBody(getString(R.string.carbohydrates_body));
+                itemWelcome.setIcon(R.drawable.ic_carbohydrates);
+                break;
+            case EXERCISE:
+                itemWelcome.setTitle(getString(R.string.exercise_title));
+                itemWelcome.setBody(getString(R.string.exercise_body));
+                itemWelcome.setIcon(R.drawable.ic_exercise);
+                break;
+            case MOOD:
+                itemWelcome.setTitle(getString(R.string.mood_title));
+                itemWelcome.setBody(getString(R.string.mood_body));
+                itemWelcome.setIcon(R.drawable.ic_mood);
+                break;
+            default:
+                itemWelcome.setTitle(getString(R.string.welcome_title));
+                itemWelcome.setBody(getString(R.string.welcome_body));
+                itemWelcome.setIcon(R.drawable.ic_mood);
+        }
 
-        itemWelcome.setTitle(getString(R.string.welcome_title));
-        itemWelcome.setBody(getString(R.string.welcome_body));
-        itemWelcome.setIcon(R.drawable.ic_mood);
+        return itemWelcome;
+    }
 
-        itemMedicine.setTitle(getString(R.string.medication_title));
-        itemMedicine.setBody(getString(R.string.medication_body));
-        itemMedicine.setIcon(R.drawable.ic_medicine);
-
-        itemDiet.setTitle(getString(R.string.diet_title));
-        itemDiet.setBody(getString(R.string.diet_body));
-        itemDiet.setIcon(R.drawable.ic_food);
-
-        itemCarbohydrates.setTitle(getString(R.string.carbohydrates_title));
-        itemCarbohydrates.setBody(getString(R.string.carbohydrates_body));
-        itemCarbohydrates.setIcon(R.drawable.ic_carbohydrates);
-
-        itemExercise.setTitle(getString(R.string.exercise_title));
-        itemExercise.setBody(getString(R.string.exercise_body));
-        itemExercise.setIcon(R.drawable.ic_exercise);
-
-        itemMood.setTitle(getString(R.string.mood_title));
-        itemMood.setBody(getString(R.string.mood_body));
-        itemMood.setIcon(R.drawable.ic_mood);
-
-        welcomeItemArrayList.add(itemWelcome);
-        welcomeItemArrayList.add(itemMedicine);
-        welcomeItemArrayList.add(itemDiet);
-        welcomeItemArrayList.add(itemCarbohydrates);
-        welcomeItemArrayList.add(itemExercise);
-        welcomeItemArrayList.add(itemMood);
-
+    private ArrayList<WelcomeItem> createItems() {
+        ArrayList<WelcomeItem> welcomeItemArrayList = new ArrayList<>();
+        welcomeItemArrayList.add(createWelcomeItem(DEFAULT));
+        welcomeItemArrayList.add(createWelcomeItem(MEDICINE));
+        welcomeItemArrayList.add(createWelcomeItem(DIET));
+        welcomeItemArrayList.add(createWelcomeItem(CARBOHYDRATES));
+        welcomeItemArrayList.add(createWelcomeItem(EXERCISE));
+        welcomeItemArrayList.add(createWelcomeItem(MOOD));
         return welcomeItemArrayList;
+    }
+
+    @Override
+    public void onItemAdded() {
+        if (mWelcomeAdapter.getItemCount() < 6) {
+            mRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    mWelcomeAdapter.addItem(createWelcomeItem(mWelcomeAdapter.getItemCount()));
+                }
+            };
+
+            mHandler = new Handler();
+            mHandler.postDelayed(mRunnable, 2000);
+        } else {
+            mHandler.removeCallbacks(mRunnable);
+        }
+
     }
 }
